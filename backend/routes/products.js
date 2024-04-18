a87bfb4798e17a2dfa020da1fd0a31f7262a3d71
Eko Firlianto <eko.firlianto@gmail.com>
2024-04-19 00:50:00 +0700

    Progres hari pertama

---
+++ backend/routes/products.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  deleteProduct,
+  getProductDetails,
+  getProducts,
+  newProduct,
+  updateProduct,
+} from "../controllers/productControllers.js";
+const router = express.Router();
+import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
+
+router.route("/products").get(getProducts);
+router
+  .route("/admin/products")
+  .post(isAuthenticatedUser, authorizeRoles("admin"), newProduct);
+
+router.route("/products/:id").get(getProductDetails);
+
+router
+  .route("/admin/products/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateProduct);
+router
+  .route("/admin/products/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteProduct);
+
+export default router;
