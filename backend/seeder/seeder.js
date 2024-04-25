178144887e913d13409401a1d398642476711973
Eko Firlianto <eko.firlianto@gmail.com>
2024-04-25 15:50:05 +0700

    Progres hari ke delapan

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,6 +4,9 @@ import Product from "../models/product.js";
 
 const seedProducts = async () => {
   try {
+    // await mongoose.connect(
+    //   "mongodb+srv://ekofirlianto:Firlianto11@cluster0.5nrl3t7.mongodb.net/shopit-v2?retryWrites=true&w=majority&appName=Cluster0"
+    // );
     await mongoose.connect("mongodb://127.0.0.1:27017/shopit-v2");
 
     await Product.deleteMany();
