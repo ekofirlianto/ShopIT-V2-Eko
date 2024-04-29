47b85de05f20411c6eb9f4e8daf17db4c7c21bca
Eko Firlianto <eko.firlianto@gmail.com>
2024-04-29 08:02:33 +0700

    Ready deploy

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,10 +4,10 @@ import Product from "../models/product.js";
 
 const seedProducts = async () => {
   try {
-    // await mongoose.connect(
-    //   "mongodb+srv://ekofirlianto:Firlianto11@cluster0.5nrl3t7.mongodb.net/shopit-v2?retryWrites=true&w=majority&appName=Cluster0"
-    // );
-    await mongoose.connect("mongodb://127.0.0.1:27017/shopit-v2");
+    await mongoose.connect(
+      "mongodb+srv://ekofirlianto:Firlianto11@ekoshopitv2.vsplvkq.mongodb.net/shopit-v2?retryWrites=true&w=majority&appName=ekoshopitv2"
+    );
+    // await mongoose.connect("mongodb://127.0.0.1:27017/shopit-v2");
 
     await Product.deleteMany();
     console.log("Products are deleted");
